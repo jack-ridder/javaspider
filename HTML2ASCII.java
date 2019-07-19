de45aee8547722bbb59cbc7c72389f90d7982f26
jack-ridder <935758882@qq.com>
2019-07-19 11:38:14 +0800

    Update HTML2ASCII.java

--- HTML2ASCII.java
+++ HTML2ASCII.java
@@ -4,26 +4,19 @@ public class HTML2ASCII extends FilterInputStream
 {
 	public HTML2ASCII(InputStream in){
 		super(in);
-	}
-
-	/**
-		Metode que llegeix d'un InputStream filtrant els tags que estan entre "<" i ">".
-		@return retorna l'enter llegit
-	*/
-
 	public int read() throws IOException{
 		int b = in.read();
 		boolean flag1=false, flag2=false, nocomm=false;
 		while (b == 60){
 			b=in.read();
 			if(b==-1) return b;
-			if(b==33){			//   Caracter !
+			if(b==33){
 			  b=in.read();
 			  if(b==-1) return b;
-			  if(b==45){			//   Caracter -
+			  if(b==45){
 				b=in.read();
 				if(b==-1) return b;
-				if(b==45){			//   Caracter -
+				if(b==45){
 					do{
 						b=in.read();
 						if(b==-1) return b;
